14c03b0e62a9ae9f0a88a7da88533669bea4c16e
Dan Snider <dansnider@gmail.com>
2015-05-24 18:15:55 -0400

    basic scaffolding

--- app/assets/javascripts/jokes.js
+++ app/assets/javascripts/jokes.js
@@ -5,16 +5,20 @@ var tweetUrl = "/tweet.json",
 		
 
 loadTweet = function(){
-	$('#joke-body').empty();
-	$('#comedian-name').empty();
+	$('#joke-body').empty().fadeOut();
+	$('#name').empty().fadeOut();
+	$('#screen-name').empty().fadeOut();
+	$('#avatar-container').empty().fadeOut();
+	$('.avatar').remove().fadeOut();
 	$.ajax({
 	  method: "GET",
 	  dataType: "json",
 	  url: tweetUrl,
 	  success: function(data){
-	  	console.log(data)
-	  	$('#comedian-name').append(data.comedian_name)
-	  	$('#joke-body').append(data.text)
+	  	$('#name').before('<img class="avatar" src="' + data.comedian_image_url + '"/>').hide().fadeIn();
+	  	$('#name').append(data.comedian_name).hide().fadeIn();
+	  	$('#screen-name').append("@" + data.comedian_screen_name).hide().fadeIn()
+	  	$('#joke-body').append(data.text).hide().fadeIn();
 	  }
 	})
 }
