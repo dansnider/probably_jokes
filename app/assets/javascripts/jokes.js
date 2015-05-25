a30b36ee52118d24e7889192d7ec37c86d6dfbf2
Dan Snider <dansnider@gmail.com>
2015-05-25 15:30:56 -0400

    self.filter method added

--- app/assets/javascripts/jokes.js
+++ app/assets/javascripts/jokes.js
@@ -10,15 +10,19 @@ loadTweet = function(){
 	$('#screen-name').empty().fadeOut();
 	$('#avatar-container').empty().fadeOut();
 	$('.avatar').remove().fadeOut();
+	$('.icon-text').empty().fadeOut();
 	$.ajax({
 	  method: "GET",
 	  dataType: "json",
 	  url: tweetUrl,
 	  success: function(data){
+	  	console.log(data);
 	  	$('#name').before('<img class="avatar" src="' + data.comedian_image_url + '"/>').hide().fadeIn();
 	  	$('#name').append(data.comedian_name).hide().fadeIn();
 	  	$('#screen-name').append("@" + data.comedian_screen_name).hide().fadeIn()
 	  	$('#joke-body').append(data.text).hide().fadeIn();
+	  	$('#rt-count').append(data.retweet_count).hide().fadeIn();
+	  	$('#fav-count').append(data.favorite_count).hide().fadeIn();
 	  }
 	})
 }
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,19 @@ class TweetsController < ApplicationController
 
 	end
 
+	def self.filter(collection)
+  	collection.each do |tweet|
+  		if tweet.text.match(/\bpm|ticket|weekend|tonight|tonite|2night|2nite|tomorrow|season|watch|special|episode|show|comedycentral|trailer|album\b/i)
+  			tweet.destroy!
+  		elsif tweet.text.starts_with?(".", "@")
+  			tweet.destroy!
+  		elsif tweet.favorite_count.to_i < 30
+  			tweet.destroy!
+  		elsif tweet.text.length.to_i < 45
+  			tweet.destroy!
+  		end
+  	end
+  	return collection
+  end
+
 end
