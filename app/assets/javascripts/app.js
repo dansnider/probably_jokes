744a08ea419c5f8f6c9f486a63a8cbfe19893bd3
Dan Snider <dansnider@gmail.com>
2016-05-22 00:01:56 -0400

    changed file structor for scss files

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,12 +2,12 @@ angular.module('probablyJokes', ['ngSanitize'])
 .controller('TweetController', function($scope, $compile) {
 
 	$scope.addTweet = function() {
-		var el = $compile( "<tweet-card class='b-card'></tweet-card>" )($scope);
-		$('.b-card_container').prepend(el);
+		var el = $compile( "<tweet-card class='card'></tweet-card>" )($scope);
+		$('.card-container').prepend(el);
 	}
 
 	$scope.$on('cardShift', function(){
-		$('tweet-card').last().addClass('b-card--offscreen');
+		$('tweet-card').last().addClass('is-offscreen');
 		setTimeout(function() { $('tweet-card').last().remove(); }, 300);
 		$scope.addTweet();
 	});
